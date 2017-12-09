4c734412b72904524569635b7714e73b7cfa59ae
Yazhi Karman <dyazhi@gmail.com>
2017-12-08 20:13:45 -0800

    Add login component still

---
+++ public/src/components/login.jsx
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+
+class Login extends Component {
+  render() {
+    const { errorMessage } = this.props;
+    return (<div>hello</div>)
+  }
+}
+
+export default Login;
