cf1f8da2f065f0487997dd24418cdff0fdc6d048
Yazhi Karman <dyazhi@gmail.com>
2017-12-09 11:14:03 -0800

    Add login container to App

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import SearchContainer from '../containers/search_c.jsx';
 import ChartContainer from '../containers/chart_c.jsx';
+import NameList from '../containers/nameList.js';
+import SearchContainer from '../containers/searchContainer.jsx';
+import Login from '../containers/Login_c.jsx'
 
 function sum(a, b) {
   return a + b;
@@ -12,9 +15,9 @@ class App extends React.Component {
       <div>
         <SearchContainer />
         <ChartContainer />
+        <Login />
       </div>
       )
-
   }
 }
 module.exports.sum = sum;
