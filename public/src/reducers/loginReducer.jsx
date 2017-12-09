f2783155e729705bf6873b096f39f3e19bb06ebb
Yazhi Karman <dyazhi@gmail.com>
2017-12-09 02:13:08 -0800

    Add and combine auth, and quotes reducer into quotesApp

--- public/src/reducers/loginReducer.jsx
+++ public/src/reducers/loginReducer.jsx
@@ -1,1 +1,69 @@
+import { combineReducers } from 'redux'
 import {SIGNUP_REQUEST, SINGUP_SUCCESS, SIGNUP_FAILIURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_SUCCESS} from './actions/actions.jsx';
+
+function auth(state ={
+  isTetching: false,
+  isAuthenticated: localStorage.getItem('id_token') ? true : false
+}, action) {
+  switch (action.type) {
+    case SIGNUP_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isAuthenticated: false,
+        user: action.creds
+      })
+    case SIGNUP_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      })
+    case SIGNUP_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      })
+    case LOGIN_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isAuthenticated: false,
+        user: action.creds
+      })
+    case LOGIN_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      })
+    case LOGIN_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      })
+    case LOGOUT_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isAuthenticated: false
+      })
+    default:
+      return state
+  }
+};
+
+//Quotes reducer
+function quotes(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+//Combine login reducers
+const quotesApp = combineReducers({
+  auth,
+  quotes
+})
+
+export default quotesApp;
