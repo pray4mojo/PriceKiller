5f2f913cf13464535bd05067ca9beaf871201a66
Yazhi Karman <dyazhi@gmail.com>
2017-12-09 01:19:48 -0800

    Add login and logout actions

--- public/src/actions/main_a.jsx
+++ public/src/actions/main_a.jsx
@@ -39,3 +39,88 @@ export function submitSearch(searchQuery) {
   }
   //return { type: SUBMIT_SEARCH, searchQuery }
 }
+
+
+
+
+
+/**** LOGIN ACTIONS ****/
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export function requestLogin(creds) {
+  return {
+    type: LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  }
+};
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export function receiveLogin(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+  }
+};
+
+export const  LOGIN_FAILURE = 'LOGIN_FAILURE';
+export function loginError(message) {
+  return {
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  }
+};
+
+/**** LOGIN API CALL ****/
+//Calls the API go get a token and dispatches along the way
+export function loginUser(creds) {
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+    body: `username=${creds.username}&password=${creds.password}`
+  };
+
+  return dispatch => {
+    dispatch(requestLogin(creds));
+  }
+}
+
+/**** LOGOUT ACTIONS ****/
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export function requestLogout() {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true
+  }
+};
+
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export function receiveLogout() {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false
+  }
+};
+
+export function logoutUser() {
+  return dispatch => {
+    dispatch(requestLogout());
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('access_token');
+    dispatch(receiveLogout());
+  }
+};
+
+
+
+export const ADD_NAME = 'ADD_NAME';
+export function addName(name) {
+  return { type: ADD_NAME, name }
+}
+
