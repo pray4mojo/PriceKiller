40fcd3941375c7fb9e2b13234cd9c757fc5aa456
Brian <bbinder912@gmail.com>
2017-12-08 19:56:34 -0500

    Add middleware to monitor state

--- public/src/actions/actions.jsx
+++ public/src/actions/actions.jsx
@@ -1,20 +1,22 @@
-export const REQUEST_RESULTS = 'REQUEST_RESULTS';
-export const RESULTS_RETURNED = 'RESULTS_RETURNED';
-export const SUBMIT_SEARCH = 'SUBMIT_SEARCH';
-export const ADD_NAME = 'ADD_NAME';
+import axios from 'axios';
 
+
+export const REQUEST_RESULTS = 'REQUEST_RESULTS';
 export function requestResults(searchQuery) {
   return { type: REQUEST_RESULTS, searchQuery }
 }
 
+export const RESULTS_RETURNED = 'RESULTS_RETURNED';
 export function resultsReturned(searchResults) {
   return { type: RESULTS_RETURNED, searchResults }
 }
 
+export const SUBMIT_SEARCH = 'SUBMIT_SEARCH';
 export function submitSearch(searchQuery) {
   return { type: SUBMIT_SEARCH, searchQuery }
 }
 
+export const ADD_NAME = 'ADD_NAME';
 export function addName(name) {
   return { type: ADD_NAME, name }
 }
--- public/src/store.js
+++ public/src/store.js
@@ -1,14 +1,27 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import mainReducer from './reducers/main.js';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
 let store;
 if (process.env.NODE_ENV === 'production') {
-  store = createStore(mainReducer, {names: ['yazhi', 'brian', 'luke']});
+  store = createStore(
+    mainReducer,
+    {names: ['yazhi', 'brian', 'luke']}
+  );
 } else {
-  store = createStore(mainReducer, devToolsEnhancer());
+  store = createStore(
+    mainReducer,
+    compose(
+      applyMiddleware(
+        createLogger(),
+        thunkMiddleware
+      ),
+      devToolsEnhancer()
+    )
+  )
 }
-store.subscribe(() => {
-  console.log('store change: ', store.getState())
-});
+
 
 export default store;
