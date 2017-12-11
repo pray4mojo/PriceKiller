246c692429851e3f4c8097ea65d86f075c7e7e86
Yazhi Karman <dyazhi@gmail.com>
2017-12-11 00:17:53 -0800

    change login reducer name

--- public/src/reducers/main_r.js
+++ public/src/reducers/main_r.js
@@ -1,8 +1,9 @@
 import searchQuery from './searchQuery_r.jsx';
 import searchResults from './searchResults_r.jsx';
 import priceHistory from './priceHistory_r.jsx';
+import auth from './Auth_r.jsx';
 import { combineReducers } from 'redux'
 
-const mainReducer = combineReducers({ searchQuery, searchResults, priceHistory });
+const mainReducer = combineReducers({ searchQuery, searchResults, priceHistory auth });
 
 export default mainReducer;
