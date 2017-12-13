43df9a55c63070704437f052622855094e48724b
Yazhi Karman <dyazhi@gmail.com>
2017-12-12 16:22:52 -0800

    Add signup field

--- public/src/components/Navbar.jsx
+++ public/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { Component, PropTypes} from 'react';
 import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 import Logout from './Logout.jsx';
-import { loginUser, logoutUser } from '../actions/auth_a.jsx';
+import { login, loginUser, signupUser, logoutUser } from '../actions/auth_a.jsx';
 
 class Navbar extends Component {
   render() {
@@ -11,7 +12,8 @@ class Navbar extends Component {
       <div className="container-fluid">
         <a className="navbar-band" href="#">Price Killer</a>
         <div className="navbar-form">
-          {!isAuthenticated && <Login errorMessage={errorMessage} onLoginClick={creds => dispatch(loginUser(creds))}/>}
+          {!isAuthenticated && <Login errorMessage={errorMessage} onLoginClick={creds => dispatch(loginUser(creds))} onGoogleLoginClick={creds => dispatch(login(creds))}/>}
+          {!isAuthenticated && <Signup errorMessage={errorMessage} onSignupClick={creds => dispatch(signupUser(creds))} />}
           {isAuthenticated && <Logout onLogoutClick={() => dispatch(logoutUser())}/>}
         </div>
       </div>
