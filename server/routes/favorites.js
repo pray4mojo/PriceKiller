69639f72879ed605f0a6d6d4e81f70c8dd1027de
Brian <bbinder912@gmail.com>
2017-12-13 11:57:32 -0500

    Add delete function to saved favorites

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -38,12 +38,31 @@ router.post('/:username', (req, res, next) => {
         });
         res.send(user.favorites);
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   });
   // {
   //   queryString: 'playstation',
   //   categoryId: 23413245324
   // }
+});
+
+router.delete('/:username', (req, res, next) => {
+  const username = req.params.username;
+  const favoriteToDelete = req.body;
+  db.User.findOne({ username })
+    .then((user) => {
+      user.favorites = user.favorites.filter((favorite) => {
+        return !((favorite.searchQuery === favoriteToDelete.searchQuery && favorite.categoryId === favoriteToDelete.categoryId))
+      });
+      user.markModified('favorites');
+      user.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      res.send(user.favorites);
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports.favorites = router;
