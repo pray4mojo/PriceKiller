159285d5e47b0e59323c657d8ebbfee70bb8a12a
Yazhi Karman <dyazhi@gmail.com>
2018-01-04 10:39:34 -0800

    Remove class name

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -10,7 +10,7 @@ function sum(a, b) {
 class App extends React.Component {
   render(){
     return (
-      <div className="section">
+      <div className="">
         <Navbar />
         <img src="https://s3-us-west-1.amazonaws.com/hackreactor27/PriceKiller_pricegun.png" alt="" width="480" height="240"/>
       </div>
