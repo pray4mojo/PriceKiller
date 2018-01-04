77996e1371bd6fe6d2c7638fbdade81cc3bf771a
Brian <bbinder912@gmail.com>
2018-01-03 22:54:24 -0500

    Display temp favorites message only once

--- public/src/components/EbayItem.jsx
+++ public/src/components/EbayItem.jsx
@@ -17,7 +17,9 @@ const mapDispatchToProps = (dispatch) => {
         $(e.target).removeClass('has-text-black');
         $(e.target).addClass('has-text-warning');
         dispatch(addNewFavorite(item));
-        alert('You just added an item to your temporary favorites list! If you would like to keep it and monitor its pricing status you must open the sidebar and click the "Store New Favorite(s)" button.');
+        if (Object.keys(isFavorited).length === 0) {
+          alert('You just added an item to your temporary favorites list! If you would like to keep it and monitor its pricing status you must open the sidebar and click the "Store New Favorite(s)" button.');
+        }
       } else {
         $(e.target).removeClass('has-text-warning');
         $(e.target).addClass('has-text-black');
